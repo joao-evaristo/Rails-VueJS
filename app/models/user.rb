a19bef381929bb722632736a5907e77d04f9c7e0
joao-evaristo <joao.evaristojw@gmail.com>
2022-11-01 15:04:21 -0300

    criar usuario/ login/ sessao

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
     has_secure_password
 
-    validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create, message: 'Precisa ser um endereço de email valido'}
+    validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create, message: 'precisa ser um endereço de email válido'}
 end
